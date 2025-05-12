1a9ed3089a63e8902fa6b7083c1993e42f143693
chanlee <cli1004@163.com>
2025-05-12 17:29:21 +0800

    修改脚本

--- add_metadata.sh
+++ add_metadata.sh
@@ -2,6 +2,6 @@
 
 
 python3 add_metadata.py \
-    --eval-problems-paths /4T/chenli/data/uf50-218/test \
+    --eval-problems-paths /4T/chenli/data/flat200-479/test \
     --train_time_max_decisions_allowed 500 \
     --test_time_max_decisions_allowed 500 \
